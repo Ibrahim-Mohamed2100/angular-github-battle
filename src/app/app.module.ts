b755ad4e00cc62c80a5f075efa1388d9ad20d1d6
Ibrahim-Mohamed2100 <ibrahim.mohamed.abdalah@gmail.com>
2021-11-27 17:30:51 +0200

    Set shared navigator with router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/main.module').then(m => m.MainModule)
+  },
   {
     path: '**',
     redirectTo: 'popular',
     pathMatch: 'full'
-  },
-  {
-    path: '',
-    loadChildren: () => import('./modules/main.module').then(m => m.MainModule)
   }
 ];
 @NgModule({
   declarations: [AppComponent],
-  imports: [RouterModule.forRoot(routes, { useHash: false }), BrowserModule],
+  imports: [RouterModule.forRoot(routes, { useHash: false }), BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [RouterModule]
--- src/app/modules/main.module.ts
+++ src/app/modules/main.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PopularComponent } from './popular/popular.component';
+import { BattleComponent } from './battle/battle.component';
 
 const routes: Routes = [
   {
     path: 'popular',
     component: PopularComponent
+  },
+  {
+    path: 'battle',
+    component: BattleComponent
   }
 ];
 @NgModule({
-  declarations: [PopularComponent],
+  declarations: [PopularComponent, BattleComponent],
   imports: [ RouterModule.forChild(routes)],
   providers: [],
   exports: [RouterModule]
